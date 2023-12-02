51c970da50966f73c622d91e1e2445bbb0ff2a30
Goby-Wan <6798988+Goby-Wan@users.noreply.github.com>
2023-12-02 19:12:46 +0100

    refactor and cleanup

--- 1/C#/goby/Ex1.cs
+++ 1/C#/goby/Ex1.cs
@@ -1,52 +1,52 @@
-using System;
 using System.Text.RegularExpressions;
 
-namespace Goby
+namespace Goby_AoC_2023;
+
+public class Ex1
 {
-  class Ex1
-  {
-    static int Main(string[] args)
+    public static int Ex1Resolve(string input)
     {
-      string inputPath = "input/day1.txt";
-      string input = File.ReadAllText(inputPath);
-      int result = 0;
+        int result = 0;
+        // Sanitize worded numbers
+        input = sanitizeWords(input);
 
-      // Sanitize worded numbers
-      input = sanitizeWords(input);
+        // Split from lines
+        string[] lines = input.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
 
-      // Split from lines
-      string[] lines = input.Split(
-        new string[] { "\r\n", "\r", "\n" },
-        StringSplitOptions.None);
+        // Get sum of first and last digits
+        foreach (string line in lines)
+        {
+            string nums = Regex.Replace(line, "\\D", String.Empty);
+            int lineNumber = (nums[0] - '0') * 10 + (nums[nums.Length - 1] - '0');
+            result += lineNumber;
+            Console.WriteLine(line + " -> " + nums + " = " + lineNumber + " -> " + result);
+        }
 
-      // Get sum of first and last digits
-      foreach (string line in lines) {
-        string nums = Regex.Replace(line, "\\D", String.Empty);
-        int lineNumber = (nums[0] - '0') * 10 + (nums[nums.Length-1] - '0');
-        result += lineNumber ;  
-        Console.WriteLine(line + " -> " + nums + " = " + lineNumber + " -> " + result);
-      }
+        Console.WriteLine("--------------");
+        Console.WriteLine(result);
 
-      return result;
+        return result;
     }
 
-    string sanitizeWords(string input) {
-      (string,string)[] words = [
-        ("one", "o1e"),
-        ("two", "t2o"),
-        ("three", "th3ee"),
-        ("four", "f4ur"),
-        ("five", "f5ve"),
-        ("six", "s6x"),
-        ("seven", "se7en"),
-        ("eight", "ei8ht"),
-        ("nine", "n9ne")
-      ];
-      foreach((string a, string b) in words) {
-        input = input.Replace(a, b);
-      }
+    static string sanitizeWords(string input)
+    {
+        (string, string)[] words =
+        [
+            ("one", "o1e"),
+            ("two", "t2o"),
+            ("three", "th3ee"),
+            ("four", "f4ur"),
+            ("five", "f5ve"),
+            ("six", "s6x"),
+            ("seven", "se7en"),
+            ("eight", "ei8ht"),
+            ("nine", "n9ne")
+        ];
+        foreach ((string a, string b) in words)
+        {
+            input = input.Replace(a, b);
+        }
 
-      return input;
+        return input;
     }
-  }
 }
