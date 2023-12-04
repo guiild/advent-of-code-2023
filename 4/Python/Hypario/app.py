67d4d370f1d50dd6bcd1d48b0ef910fed8b4b80d
Hypario <fabienduterte@mailfence.com>
2023-12-04 13:44:11 +0100

    added part 2 of day 4

--- 4/Python/Hypario/app.py
+++ 4/Python/Hypario/app.py
@@ -5,13 +5,18 @@ class Card():
     scratched_numbers: set[int]
     copies: int
     
-    def __init__(self, winning_numbers: set[int], scratched_numbers: set[int], copies: int) -> None:
+    def __init__(self, winning_numbers: set[int], scratched_numbers: set[int],) -> None:
         self.winning_numbers = winning_numbers
         self.scratched_numbers = scratched_numbers
-        self.copies = copies
+        self.copies = 1
+    
+    def number_of_matches(self) -> int:
+        return len(self.winning_numbers & self.scratched_numbers)
+    
+    def increment_copy_by(self, increment):
+        self.copies += increment
 
 def parse_input(lines: list[str]) -> list[Card]:
-    # returns the number of winning numbers
     numbers = [line.split(":")[1].strip() for line in lines]
     
     cards = []
@@ -24,20 +29,24 @@ def parse_input(lines: list[str]) -> list[Card]:
         for match in re.finditer(r"\d+", scratched):
             scratched_set.add(int(match.group()))
         
-        cards.append(Card(winning_set, scratched_set, 1))
+        cards.append(Card(winning_set, scratched_set))
     
     return cards
-
-def calculate_points(card: Card) -> int:
-    power = len(card.winning_numbers & card.scratched_numbers)
-    return int(2 ** (power - 1))
 
 def main():
     file = open("input.txt", "r")
     lines = file.readlines()
     cards = parse_input([line.rstrip("\n") for line in lines])
     
-    print(sum([calculate_points(card) for card in cards]))  # part 1
+    print(sum([int(2 ** (card.number_of_matches() - 1)) for card in cards]))  # part 1
+    
+    # part 2
+    for card_number, card in enumerate(cards):
+        matches = card.number_of_matches()
+        for i in range(matches):
+            cards[card_number + 1 + i].increment_copy_by(cards[card_number].copies)
+    
+    print(sum([card.copies for card in cards]))  # part 2
 
 if __name__ == "__main__":
     main()
