845d773bc366f4a8f4fb8cb9da13201a91f79168
Hypario <fabienduterte@mailfence.com>
2023-12-04 13:54:38 +0100

    add comments

--- 4/Python/Hypario/app.py
+++ 4/Python/Hypario/app.py
@@ -23,11 +23,11 @@ class Card():
         self.copies += increment
 
 def parse_input(lines: list[str]) -> list[Card]:
-    numbers = [line.split(":")[1].strip() for line in lines]
+    numbers = [line.split(":")[1].strip() for line in lines]  # get all numbers for each cards
     
     cards = []
     
-    for line in numbers:
+    for line in numbers:  # for each cards, separates the winning numbers, and the number we got
         winning, scratched = line.split("|")
         winning_set, scratched_set = set(), set()
         for match in re.finditer(r"\d+", winning):
@@ -35,6 +35,7 @@ def parse_input(lines: list[str]) -> list[Card]:
         for match in re.finditer(r"\d+", scratched):
             scratched_set.add(int(match.group()))
         
+        #  add our card with their respective values to a list
         cards.append(Card(winning_set, scratched_set))
     
     return cards
@@ -45,15 +46,18 @@ def main():
     file.close()
     cards = parse_input([line.rstrip("\n") for line in lines])
     
+    # the points are calculated as follow 1 * 2 * 2 * ... * n which can be simplified as 2^(n-1)
     print(sum([int(2 ** (card.number_of_matches() - 1)) for card in cards]))  # part 1
     
     # part 2
+    
+    # for each card we increment a card's copies by the copies for each match
     for card_number, card in enumerate(cards):
         matches = card.number_of_matches()
         for i in range(matches):
             cards[card_number + 1 + i].increment_copy_by(cards[card_number].copies)
     
-    print(sum([card.copies for card in cards]))  # part 2
+    print(sum([card.copies for card in cards])) 
 
 if __name__ == "__main__":
     main()
