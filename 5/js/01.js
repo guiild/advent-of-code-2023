0360fd91e4222e33964796721f56258096a3cfe7
Orval Touitou <orval.touitou@guiild.fr>
2023-12-05 12:11:36 +0100

    Day 5

--- 5/js/01.js
+++ 5/js/01.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const input = fs.readFileSync('./input', {
+const [seedsStr, ...mapsStr] = input = fs.readFileSync('./input', {
   encoding: 'utf8',
   flag: 'r'
 })
@@ -8,28 +8,12 @@ const input = fs.readFileSync('./input', {
   .trim()
   .split('\n\n')
 
-const seeds = input[0].split(':')[1].split(' ').filter(it => it !== '').map(it => +it)
-
-const parseMap = (str) => str.split('\n').slice(1).map(it => it.split(' ').map(it => +it).filter(it => !isNaN(it)))
-
-const seedToSoil = parseMap(input[1])
-const soilToFertilizer = parseMap(input[2])
-const fertilizerToWater = parseMap(input[3])
-const waterToLight = parseMap(input[4])
-const lightToTemperature = parseMap(input[5])
-const temperatureToHumidity = parseMap(input[6])
-const humidityToLocation = parseMap(input[7])
+const seeds = seedsStr.split(':')[1].split(' ').filter(it => it !== '').map(it => +it)
+const maps = mapsStr.map(((str) => str.split('\n').slice(1).map(it => it.split(' ').map(it => +it).filter(it => !isNaN(it)))))
 
 const mapper = (element, map) => map.filter(it => it[1] <= element && element < it[1] + it[2]).map(it => it[0] + (element - it[1]))[0] ?? element
 
 console.log(
-  seeds
-    .map(it => mapper(it, seedToSoil))
-    .map(it => mapper(it, soilToFertilizer))
-    .map(it => mapper(it, fertilizerToWater))
-    .map(it => mapper(it, waterToLight))
-    .map(it => mapper(it, lightToTemperature))
-    .map(it => mapper(it, temperatureToHumidity))
-    .map(it => mapper(it, humidityToLocation))
+  seeds.map(s => maps.reduce((acc, m) => mapper(acc, m), s))
     .reduce((a, b) => a < b ? a : b, Number.POSITIVE_INFINITY)
 )
--- 5/js/02.js
+++ 5/js/02.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const [seedsStr, ...maps] = fs.readFileSync('./input', {
+const [seedsStr, ...mapsStr] = fs.readFileSync('./input', {
   encoding: 'utf8',
   flag: 'r'
 })
@@ -9,7 +9,7 @@ const [seedsStr, ...maps] = fs.readFileSync('./input', {
   .split('\n\n')
 
 // transforms a string in the format "start1 nb1 start2 nb2" into a list of ranges like [[start1, start1 + nb1], [start2, start2 + nb2]]
-const seeds = seedsStr.split(':')[1].split(' ').filter(it => it !== '').map(it => +it).filter(it => !isNaN(it))
+const seedRanges = seedsStr.split(':')[1].split(' ').filter(it => it !== '').map(it => +it).filter(it => !isNaN(it))
   .map((s, idx, arr) => {
     if (idx % 2 == 0) return [s, s + arr[idx + 1]]
   })
@@ -17,21 +17,14 @@ const seeds = seedsStr.split(':')[1].split(' ').filter(it => it !== '').map(it =
 
 const parseMap = (str) => str.split('\n').slice(1).map(it => it.split(' ').map(it => +it).filter(it => !isNaN(it)))
 
-const [seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, temperatureToHumidity, humidityToLocation] = maps.map(parseMap)
+const maps = mapsStr.map(parseMap).toReversed()
 
 const mapper = (element, map) => map.filter(it => it[0] <= element && element < it[0] + it[2]).map(it => it[1] + (element - it[0]))[0] ?? element
 
+// Ineffective solution where we loop through all the locations to see if a seed leads to it
 for (let location = 0; ; location++) {
-  const s = [location]
-    .map(it => mapper(it, humidityToLocation))
-    .map(it => mapper(it, temperatureToHumidity))
-    .map(it => mapper(it, lightToTemperature))
-    .map(it => mapper(it, waterToLight))
-    .map(it => mapper(it, fertilizerToWater))
-    .map(it => mapper(it, soilToFertilizer))
-    .map(it => mapper(it, seedToSoil))
-    .filter(it => seeds.some(s => s[0] <= it && it <= s[1]))
-  if (s.length != 0) {
+  const s = maps.reduce((acc, m) => mapper(acc, m), location)
+  if (seedRanges.some(seedRange => seedRange[0] <= s && s < seedRange[1])) {
     console.log(location)
     break
   }
