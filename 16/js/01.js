2daf78462ecf7b84a3ce2b702b7f53233d89ecb9
Orval Touitou <orval.touitou@guiild.fr>
2023-12-16 10:22:41 +0100

    Day 16 in js

---
+++ 16/js/01.js
@@ -0,0 +1,106 @@
+const fs = require('fs')
+
+const map = fs.readFileSync('./input', {
+  encoding: 'utf8',
+  flag: 'r'
+})
+  .trim()
+  .split('\n')
+  .map(it => it.split(''))
+
+const beams = []
+const visited = {}
+
+beams.push({ x: 0, y: 0, dir: 'E' })
+
+const key = beam => `${beam.x},${beam.y}`
+
+const visit = beam => {
+  const k = key(beam)
+  if (visited[k] == null) {
+    visited[k] = {}
+  }
+  visited[k][beam.dir] = true
+}
+
+const hasBeenVisited = beam => {
+  const k = key(beam)
+  return visited[k] != null && visited[k][beam.dir]
+}
+
+const travelThrough = beam => {
+  if (beam.dir == 'E') {
+    return beam.x < map[0].length - 1 ? [{ ...beam, x: beam.x + 1 }] : []
+  }
+  if (beam.dir == 'S') {
+    return beam.y < map.length - 1 ? [{ ...beam, y: beam.y + 1 }] : []
+  }
+  if (beam.dir == 'W') {
+    return beam.x > 0 ? [{ ...beam, x: beam.x - 1 }] : []
+  }
+  if (beam.dir == 'N') {
+    return beam.y > 0 ? [{ ...beam, y: beam.y - 1 }] : []
+  }
+}
+
+const turn90DegClockwise = beam => {
+  if (beam.dir == 'E') {
+    return beam.y < map.length - 1 ? [{ ...beam, y: beam.y + 1, dir: 'S' }] : []
+  }
+  if (beam.dir == 'S') {
+    return beam.x > 0 ? [{ ...beam, x: beam.x - 1, dir: 'W' }] : []
+  }
+  if (beam.dir == 'W') {
+    return beam.y > 0 ? [{ ...beam, y: beam.y - 1, dir: 'N' }] : []
+  }
+  if (beam.dir == 'N') {
+    return beam.x < map[0].length - 1 ? [{ ...beam, x: beam.x + 1, dir: 'E' }] : []
+  }
+}
+
+const turn90DegAntiClockwise = beam => {
+  if (beam.dir == 'W') {
+    return beam.y < map.length - 1 ? [{ ...beam, y: beam.y + 1, dir: 'S' }] : []
+  }
+  if (beam.dir == 'N') {
+    return beam.x > 0 ? [{ ...beam, x: beam.x - 1, dir: 'W' }] : []
+  }
+  if (beam.dir == 'E') {
+    return beam.y > 0 ? [{ ...beam, y: beam.y - 1, dir: 'N' }] : []
+  }
+  if (beam.dir == 'S') {
+    return beam.x < map[0].length - 1 ? [{ ...beam, x: beam.x + 1, dir: 'E' }] : []
+  }
+}
+
+const travel = beam => {
+  const cell = map[beam.y][beam.x]
+  if (cell == '.') {
+    return travelThrough(beam)
+  } else if (cell == '-') {
+    if (beam.dir == 'E' || beam.dir == 'W') {
+      return travelThrough(beam)
+    } else {
+      return [...turn90DegAntiClockwise(beam), ...turn90DegClockwise(beam)]
+    }
+  } else if (cell == '|') {
+    if (beam.dir == 'N' || beam.dir == 'S') {
+      return travelThrough(beam)
+    } else {
+      return [...turn90DegAntiClockwise(beam), ...turn90DegClockwise(beam)]
+    }
+  } else if (cell == '/') {
+    return beam.dir == 'N' || beam.dir == 'S' ? turn90DegClockwise(beam) : turn90DegAntiClockwise(beam)
+  } else if (cell == '\\') {
+    return beam.dir == 'E' || beam.dir == 'W' ? turn90DegClockwise(beam) : turn90DegAntiClockwise(beam)
+  }
+}
+
+while (beams.length > 0) {
+  const beam = beams.pop()
+  if (hasBeenVisited(beam)) continue
+  visit(beam)
+  beams.push(...travel(beam))
+}
+
+console.log(Object.keys(visited).length)
